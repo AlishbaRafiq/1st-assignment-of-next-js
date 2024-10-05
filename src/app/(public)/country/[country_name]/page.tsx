989bf8c524bfc448395ad93439a39eff16abcad4
AlishbaRafiq <165753406+AlishbaRafiq@users.noreply.github.com>
2024-10-05 11:06:30 +0500

    Add files via upload

---
+++ src/app/(public)/country/[country_name]/page.tsx
@@ -0,0 +1,27 @@
+// app/country/[country_name]/page.tsx
+
+import { countries } from "../../data/countries";
+// import { notFound } from "next/navigation";
+
+interface CountryPageProps {
+  params: {
+    country_name: string;
+  };
+}
+
+export default function CountryPage({ params }: CountryPageProps) {
+  const country = countries[params.country_name.toLowerCase()];
+
+  if (!country) {
+    return <h1>Country not found.</h1>;
+  }
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+      <p><strong>Capital:</strong> {country.capital}</p>
+    </div>
+  );
+}
+  
